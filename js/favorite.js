ac5304704e77e2a166aef7b260011e7f4aa21df9
Dimitri <dimitri.sandron@outlook.fr>
2016-11-17 15:31:37 +0100

    modifs print favorites with checkbox for more infos

--- js/favorite.js
+++ js/favorite.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function(){
     var favListe = [];
+    var block_print = '';
     $( "#favorite" ).click(function() {
 
         console.log(favListe);
@@ -20,6 +21,13 @@ jQuery(document).ready(function(){
             $('.favListe').append(blockYear);
             var array = blockTitre + blockYear;
             localStorage.setItem(getTitre(),array);
+            // A l'ajout d'un favoris, on l'append aussi dans le tableau des favoris qui sera imprimé
+            var blockTitrePrintMore = '<tr class="favTitlePrint"><td><strong>'+ getTitre()+'</strong></td><td><strong>'+getRealisateur()+'</strong></td><td><strong>'+getYear()+'</strong></td></tr><tr class="favTitlePlot"><td colspan="3">'+ getSynopsis()+'</td></tr>';
+            var blockTitrePrintLess = '<tr class="favTitlePrint"><td><strong>'+ getTitre()+'</strong></td></tr>';
+            $('.favListePrintMore').append(blockTitrePrintMore);
+            $('.favListePrintLess').append(blockTitrePrintLess);
+            localStorage.setItem('block_print_more', $('.favListePrintMore').html());  // Stockage de la div d'impression pour la retrouver au chargement de la page
+            localStorage.setItem('block_print_less', $('.favListePrintLess').html());  // Stockage de la div d'impression pour la retrouver au chargement de la page
         }
 
     });
@@ -31,6 +39,14 @@ jQuery(document).ready(function(){
     function getYear(){
         var year = $('#released').text();
         return year;
+    }
+    function getRealisateur(){
+        var director = $('#director').text();
+        return director;
+    }
+    function getSynopsis(){
+        var synopsis = $('#plot').text();
+        return synopsis;
     }
 
 });
